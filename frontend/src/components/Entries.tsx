f776a42b54e2ff120d6f24f57643f3ff212c9ebb
Alex Aleksandrov <aleckaleksandrov@gmail.com>
2023-12-30 15:40:49 -0800

    Added textbox for annotation

--- frontend/src/components/Entries.tsx
+++ frontend/src/components/Entries.tsx
@@ -3,6 +3,8 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 import {formatDateString} from "../utils/utils";
 
 function EntriesComponent() {
@@ -31,19 +33,34 @@ function EntriesComponent() {
     }, []);
 
     return (
-        <Box sx={{ margin: 2 }}>
-            {events.map((event: any, index: number) => (
-                <Card key={index} sx={{ marginBottom: 2, backgroundColor: '#f5f5f5' }}>
-                    <CardContent>
-                        <Typography variant="h6">
-                            {event.name}
-                        </Typography>
-                        <Typography color="textSecondary">
-                            {formatDateString(event.date)}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            ))}
+        <Box sx={{ flexGrow: 1, margin: 2 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    {events.map((event: any, index: any) => (
+                        <Card key={index} sx={{ marginBottom: 2, backgroundColor: '#f5f5f5' }}>
+                            <CardContent>
+                                <Typography variant="h6">
+                                    {event.name}
+                                </Typography>
+                                <Typography color="textSecondary">
+                                    {formatDateString(event.date)}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </Grid>
+                <Grid item xs={9}>
+                    <TextField
+                        label="Notes"
+                        multiline
+                        rows={22}
+                        defaultValue="Add your notes here..."
+                        variant="outlined"
+                        fullWidth
+                        sx={{ height: '100%' }}
+                    />
+                </Grid>
+            </Grid>
         </Box>
     );
 }
